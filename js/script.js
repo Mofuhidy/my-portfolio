544af19b575297efcaaba58e0fdb4f467759a71e
moFuhidy <mo7ammed9290@gmail.com>
2023-05-15 19:22:16 +0300

    added the link functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,33 @@
-let button = document.querySelector ('.menuIcon');
-let navMenu = document.querySelector('.navInfoMobile, .cancelIcon');
-let nav = document.querySelector('.menuIcon, .logo');
-button.addEventListener('click',()=> {
-  navMenu.classList.toggle('active')
-})
+const button = document.querySelector('.menuIcon');
+const buttonCancel = document.querySelector('.cancelIcon');
+const navMenu = document.querySelector('.navInfoMobile, .cancelIcon');
+const nav = document.querySelector('.menuIcon, .logo');
+const body = document.querySelector('body');
+const mobileLinks = document.querySelectorAll('.mobileLinks');
+
+// menu mobile function
+button.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  nav.classList.toggle('dNone');
+  button.classList.toggle('dNone');
+  buttonCancel.classList.toggle('active');
+  body.classList.toggle('active');
+});
+
+buttonCancel.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+  nav.classList.remove('dNone');
+  button.classList.remove('dNone');
+  buttonCancel.classList.toggle('active');
+});
+
+// links functionality
+mobileLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
+    nav.classList.toggle('dNone');
+    button.classList.toggle('dNone');
+    buttonCancel.classList.toggle('active');
+    body.classList.toggle('active');
+  });
+});
