452434789465394c548dff400802d4622078ca2e
Mofuhidy <mo7ammed9290@gmail.com>
2023-05-17 17:53:39 +0300

    Revert "claient side validation form"

--- js/script.js
+++ js/script.js
@@ -31,24 +31,3 @@ mobileLinks.forEach((link) => {
     body.classList.toggle('active');
   });
 });
-
-// validation form
-const form = document.getElementById('contact-form');
-const email = document.getElementById('email');
-const errorMsg = document.querySelector('.error');
-const regex = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
-
-function isValid(e) {
-  e.preventDefault();
-  // cheking email value if it is the same as the regex pattern
-  if (regex.test(email.value)) {
-    form.submit();
-    form.reset();
-  } else {
-    errorMsg.innerText = 'The email should be in lower case';
-  }
-}
-
-form.addEventListener('submit', (e) => {
-  isValid(e);
-});
