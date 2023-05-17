00e2a87f1b423a1e2abc23ee45901256552d9485
moFuhidy <mo7ammed9290@gmail.com>
2023-05-17 17:42:24 +0300

    fix some linters and change position of error msg

--- js/script.js
+++ js/script.js
@@ -40,12 +40,13 @@ const regex = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
 
 function isValid(e) {
   e.preventDefault();
-if (regex.test(email.value)){
-  form.submit()
-  form.reset()
-}else {
-  errorMsg.innerText='The email should be in lower case'
-}
+  // cheking email value if it is the same as the regex pattern
+  if (regex.test(email.value)) {
+    form.submit();
+    form.reset();
+  } else {
+    errorMsg.innerText = 'The email should be in lower case';
+  }
 }
 
 form.addEventListener('submit', (e) => {
