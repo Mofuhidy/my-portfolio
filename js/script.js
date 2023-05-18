54fd24dcfa00192992f6ec18da44495f1d297756
moFuhidy <mo7ammed9290@gmail.com>
2023-05-18 11:11:36 +0300

    set the item in local storage

--- js/script.js
+++ js/script.js
@@ -32,15 +32,18 @@ mobileLinks.forEach((link) => {
   });
 });
 
-// validation form
+// validation form and local storage
 const form = document.getElementById('contact-form');
 const email = document.getElementById('email');
+const fullName = document.getElementById('full-name')
+const areaMsg = document.getElementById('text-area-msg')
 const errorMsg = document.querySelector('.error');
 const regex = /^[a-z0-9._-]+@[a-z0-9.-]+\.[a-z]{2,4}$/g;
 
 function isValid(e) {
   e.preventDefault();
   // cheking email value if it is the same as the regex pattern
+  errorMsg.innerText = '';
   if (regex.test(email.value)) {
     form.submit();
     form.reset();
@@ -49,6 +52,18 @@ function isValid(e) {
   }
 }
 
+// local storage function
+function savedData() {
+  const formData = {
+    fullname: fullName.value,
+    email: email.value,
+    areaMsg: areaMsg.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
 form.addEventListener('submit', (e) => {
   isValid(e);
 });
+
+form.addEventListener('submit', savedData);
