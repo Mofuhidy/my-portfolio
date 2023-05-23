438847d295718689b1fc064ef80acc62235558c7
Mohammed Al-Fuhidy <mo7ammed9290@gmail.com>
2023-05-23 20:37:53 +0300

    Revert "added my last work and fixed the description height"

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,12 +1,14 @@
 // main work section
 const overlay = document.querySelector('.overlay');
 const mainWork = {
-  title: 'The Arab Youth Sustainable Development Network (AYSDN)',
-  image: './imgs/works/aysdn.png',
-  live: 'https://mofuhidy.github.io/capstone-project.github.io/',
-  repository: 'https://github.com/Mofuhidy/first-capstone-project',
-  technology: ['Bootstrap', 'JS', 'HTML', 'CSS'],
-  description: 'The Arab Youth Sustainable Development Network (AYSDN) is a network launched among youth professionals and experts from the MENA working on Sustainable Development Goals. The network keen to assist local government , local and international Organizations, private sectors and to support the United Nations to achieve the 17 Sustainable Development Goals in the Arab World as well as educate youth about the Sustainable Development Goals (SDGs) and provide practical volunteering opportunities, amongst other activities, to unleash their potential while being an active world citizen.',
+  title: 'Multi-Post Stories',
+  image: './imgs/works/myWork.png',
+  live: '#',
+  repository: 'https://github.com/Mofuhidy',
+  technology: ['Bootstrap', 'Ruby', 'HTML', 'CSS'],
+  description: `A daily selection of privately personalized reads; no accounts or
+              sign-ups required. has been the industry's standard dummy text
+           ever since the 1500s, when an unknown printer took a standard dummy text.`,
 };
 
 const mainWorkSection = document.querySelector('#mainWork');
