24244166c50d854ea3ffb64c5242b27484f5fa6c
moFuhidy <mo7ammed9290@gmail.com>
2023-07-11 17:33:21 +0300

    Add links for social and projects

--- js/dynamic.js
+++ js/dynamic.js
@@ -1,12 +1,12 @@
 // main work section
 const overlay = document.querySelector('.overlay');
 const mainWork = {
-  title: 'The Arab Youth Sustainable Development Network (AYSDN)',
-  image: './imgs/works/aysdn.png',
-  live: 'https://mofuhidy.github.io/capstone-project.github.io/',
-  repository: 'https://github.com/Mofuhidy/first-capstone-project',
-  technology: ['Bootstrap', 'JS', 'HTML', 'CSS'],
-  description: 'The Arab Youth Sustainable Development Network (AYSDN) is a network launched among youth professionals and experts from the MENA working on Sustainable Development Goals. The network keen to assist local government , local and international Organizations, private sectors and to support the United Nations to achieve the 17 Sustainable Development Goals in the Arab World as well as educate youth about the Sustainable Development Goals (SDGs) and provide practical volunteering opportunities, amongst other activities, to unleash their potential while being an active world citizen.',
+  title: 'Math Magicians app',
+  image: './imgs/works/Screenshot 2023-07-11 at 17.03.25.png',
+  live: 'https://math-magicians-app-6pdl.onrender.com/',
+  repository: 'https://github.com/Mofuhidy/math-magicians',
+  technology: ['React', 'Tailwindcss'],
+  description: ' Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote',
 };
 
 const mainWorkSection = document.querySelector('#mainWork');
@@ -61,8 +61,8 @@ function mainPopUp() {
          ${mainWork.description}
         </p>
         <div class="footer">
-         <a href="${mainWork.live}"><button class="btn">See live <img src="./imgs/Icon-see-live.png" alt="see live icon"></button></a>
-         <a href="${mainWork.repository}"><button class="btn">See source <img src="./imgs/giticon.png" alt="github icon"></button></a>
+         <a href="${mainWork.live}" target="_blank"><button class="btn">See live <img src="./imgs/Icon-see-live.png" alt="see live icon"></button></a>
+         <a href="${mainWork.repository}" target="_blank"><button class="btn">See source <img src="./imgs/giticon.png" alt="github icon"></button></a>
         </div>
       </div>
     </div>
@@ -111,14 +111,12 @@ forMainModal();
 
 // works section
 const works = [{
-  title: 'Multi-Post Stories',
-  image: './imgs/works/work2.png',
-  live: '#',
-  repository: 'https://github.com/Mofuhidy',
-  technology: ['Bootstrap', 'Ruby', 'HTML', 'CSS'],
-  description: `A daily selection of privately personalized reads; no accounts or
-            sign-ups required. has been the industry's standard dummy text
-         ever since the 1500s, when an unknown printer took a standard dummy text.`,
+  title: 'The Arab Youth Sustainable Development Network (AYSDN)',
+  image: './imgs/works/aysdn.png',
+  live: 'https://mofuhidy.github.io/capstone-project.github.io/',
+  repository: 'https://github.com/Mofuhidy/first-capstone-project',
+  technology: ['Bootstrap', 'JS', 'HTML', 'CSS'],
+  description: 'The Arab Youth Sustainable Development Network (AYSDN) is a network launched among youth professionals and experts from the MENA working on Sustainable Development Goals. The network keen to assist local government , local and international Organizations, private sectors and to support the United Nations to achieve the 17 Sustainable Development Goals in the Arab World as well as educate youth about the Sustainable Development Goals (SDGs) and provide practical volunteering opportunities, amongst other activities, to unleash their potential while being an active world citizen.',
 },
 {
   title: 'Multi-Post Stories',
@@ -165,7 +163,7 @@ const works = [{
   image: './imgs/works/work1.png',
   live: '#',
   repository: 'https://github.com/Mofuhidy',
-  technology: ['Bootstrap', 'Ruby', 'HTML', 'CSS'],
+  technology: ['Bootstrap', 'React', 'HTML', 'CSS'],
   description: `A daily selection of privately personalized reads; no accounts or
             sign-ups required. has been the industry's standard dummy text
          ever since the 1500s, when an unknown printer took a standard dummy text.`,
@@ -188,7 +186,7 @@ function worksCard() {
                  </p>
                  </div>
                  <ul class="mainListCategoris">
-                 ${mainWork.technology.map((item) => `<li class="tags">${item}</li>`).join('')}
+                 ${work.technology.map((item) => `<li class="tags">${item}</li>`).join('')}
                  </ul>
                  </div>
                  <button type="button" class="btn cardBtn showPro">See project</button>
@@ -222,8 +220,8 @@ function windowPopUp() {
            ${work.description}
           </p>
           <div class="footer">
-           <a href="${work.live}"><button class="btn">See live <img src="./imgs/Icon-see-live.png" alt="see live icon"></button></a>
-           <a href="${work.repository}"><button class="btn">See source <img src="./imgs/giticon.png" alt="github icon"></button></a>
+           <a href="${work.live}" target="_blank"><button class="btn">See live <img src="./imgs/Icon-see-live.png" alt="see live icon"></button></a>
+           <a href="${work.repository}" target="_blank"><button class="btn">See source <img src="./imgs/giticon.png" alt="github icon"></button></a>
           </div>
         </div>
       </div>
